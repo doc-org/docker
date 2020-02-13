65240d1eaad03b78a9d08c174dad8c86360a27db
MarcoIeni <marcoieni94@gmail.com>
2020-02-13 21:52:54 +0100

    quote variables

--- src/run.sh
+++ src/run.sh
@@ -34,6 +34,6 @@ BUILD_DIR=build
 
 latexmk -quiet -pdf -pdflatex="pdflatex -interaction=nonstopmode" main.tex # compile
 
-mkdir $BUILD_DIR
-mv main.pdf $BUILD_DIR/$PDF_FILENAME.pdf
-mv main.fdb_latexmk main.fls main.log main.tex main.aux $BUILD_DIR
+mkdir "$BUILD_DIR"
+mv main.pdf ""$BUILD_DIR"/"$PDF_FILENAME".pdf"
+mv main.fdb_latexmk main.fls main.log main.tex main.aux "$BUILD_DIR"
