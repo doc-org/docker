9b6ac5fff25b52fd51e74c7bfd398139d47b15f0
MarcoIeni <marcoieni94@gmail.com>
2020-02-14 21:02:28 +0100

    create build dir only if it doesn't exist

--- src/run.sh
+++ src/run.sh
@@ -34,7 +34,7 @@ BUILD_DIR="build"
 
 latexmk -quiet -pdf -pdflatex="pdflatex -interaction=nonstopmode" main.tex # compile
 
-mkdir "$BUILD_DIR"
+mkdir -p "$BUILD_DIR"
 OUTPUT_PDF=""$BUILD_DIR"/"$PDF_FILENAME".pdf"
 mv main.pdf "$OUTPUT_PDF"
 mv main.fdb_latexmk main.fls main.log main.tex main.aux "$BUILD_DIR"
