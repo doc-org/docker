0a5885bcea6ddb80393341c75a152528b6873f4e
MarcoIeni <marcoieni94@gmail.com>
2020-02-15 12:43:20 +0100

    document title is optional

--- src/run.sh
+++ src/run.sh
@@ -18,7 +18,7 @@ emacs main.org \
 
 if [ "$BODY_ONLY" = "t" ]
 then
-    sed -i '1 s/^/\\input{header.tex}\n\n\\begin{document}\n\\maketitle\n/' main.tex
+    sed -i '1 s/^/\\input{header.tex}\n\n\\begin{document}\n/' main.tex
 
     echo -e "\n\\\end{document}" >> main.tex
 fi
